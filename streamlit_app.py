b10fe92ef546d9bdc3c73155c19e44187c63b9cf
David Oodugama <63865452+davidoodugama@users.noreply.github.com>
2025-04-22 15:47:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ st.write('The name on your Smoothie will be:', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-session = get_active_session()
 # my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED") == 0).collect()
 # st.dataframe(data=my_dataframe, use_container_width = True)
